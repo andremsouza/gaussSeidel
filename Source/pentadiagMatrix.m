c6bd8f8d221fcfb6f0e7c94913330fbef819d0b6
andremsouza <msouza.andre@hotmail.com>
2018-04-30 12:05:37 -0300

    Minor notation changes.

--- Source/pentadiagMatrix.m
+++ Source/pentadiagMatrix.m
@@ -2,7 +2,7 @@
 function A = pentadiagMatrix(n) # n > 0; n is an int32 variable
   if(!isinteger(n) || n <= 0) # check invalid inputs and return error
     A = "ERR_INVALID_INPUT"; return; endif;
-  A = diag(4*ones(1, n)); # main diagonal matrix
+  A = diag(4*ones(n, 1)); # main diagonal matrix
   for i = [-3,-1,1,3] # computing the "shifted" diagonals
     if(n > abs(i))
       A = A + diag(-ones(1, n - abs(i)), i); endif;
