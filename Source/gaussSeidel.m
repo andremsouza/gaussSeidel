1021565866b6e2332f39a93a09c0710e064d2675
andremsouza <msouza.andre@hotmail.com>
2018-04-30 11:13:05 -0300

    Fixed errors. Rows and columns inverted.

--- Source/gaussSeidel.m
+++ Source/gaussSeidel.m
@@ -6,20 +6,20 @@
 # Warning: convergence is only guaranteed if the matrix A is diagonally dominant
 # or if the matrix A is SPD
 
-function x = gaussSeidel(A, b, n, ep, itmax)
+function x = gaussSeidel(A=pentadiagMatrix(uint32(50)), b=(sum(A))', n=50, ep=1e-10, itmax=uint32(100))
   # Validar parametros de entrada
-  if(n <= 0 || !ismatrix(A) || numel(A) != n*n || !iscolumn(b) || numel(b) != n)
+  if(n <= 0 || !ismatrix(A) || numel(A) != n*n || !iscolumn(b) || numel(b) != n || itmax <= 0 || ep <= 0)
     x = "ERR_INVALID_INPUT"; return; endif;
   # Tomamos x = [0, 0, 0, ..., 0] como o vetor inicial "x_0"
   x = xo = zeros(n, 1);
   for k = 1:itmax
     for i = 1:n
-      x(1, i) += b(1, i) / A(i,i); # b_i term
+      x(i, 1) += b(i, 1) / A(i,i); # b_i term
       for j = 1:(i-1) # First summation
-        x(1, i) -= A(i, j) * x(1, j) / A(i, i);
+        x(i, 1) -= A(i, j) * x(j, 1) / A(i, i);
       endfor;
       for j = (i+1):n # Second summation
-        x(1, i) -= A(i, j) * xo(1, j) / A(i, i);
+        x(i, 1) -= A(i, j) * xo(j, 1) / A(i, i);
       endfor;
     endfor;
     # If we have acceptable results, based on the ep variable
