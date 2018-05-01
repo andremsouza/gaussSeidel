8cd47528b2ea7b26ead7e26499c839c0ac21f524
andremsouza <msouza.andre@hotmail.com>
2018-05-01 10:39:55 -0300

    Added errors for each result vector

--- Source/main.m
+++ Source/main.m
@@ -5,21 +5,25 @@
 format long
 
 # First test (b))
-disp("Primeiro Teste (b): ")
+disp("Primeiro Teste (b): \n")
 n = uint32(50);
 A = pentadiagMatrix(n);
 b = sum(A)';
 x = gaussSeidel(A, b, n, :, :, :)
+disp("Primeiro Teste (b) (Erro): \n")
+err = A*x - b
 
 # Second test (b))
-disp("Segundo Teste (b): ")
+disp("Segundo Teste (b): \n")
 n = uint32(100);
 A = pentadiagMatrix(n);
 b = sum(A)';
 x = gaussSeidel(A, b, n, :, :, :)
+disp("Segundo Teste (b) (Erro): \n")
+err = A*x - b
 
 # Third test (c))
-disp("Terceiro Teste (c): ")
+disp("Terceiro Teste (c): \n")
 n = uint32(100);
 A = pentadiagMatrix(n);
 for(i=1:n)
@@ -28,3 +32,5 @@ endfor
 b;
 ep = 1e-10;
 x = gaussSeidel(A, b, n, :, ep, :)
+disp("Terceiro Teste (c) (Erro): \n")
+err = A*x - b
