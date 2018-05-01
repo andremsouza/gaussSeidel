ea74ef878822aff5b1b3dc6215496994abbea72d
andremsouza <msouza.andre@hotmail.com>
2018-05-01 10:29:57 -0300

    Updated to comply with new gaussSeidel.m

--- Source/main.m
+++ Source/main.m
@@ -6,25 +6,25 @@ format long
 
 # First test (b))
 disp("Primeiro Teste (b): ")
-n = int32(50);
+n = uint32(50);
 A = pentadiagMatrix(n);
 b = sum(A)';
-x = gaussSeidel(A, b, n, :, :)
+x = gaussSeidel(A, b, n, :, :, :)
 
 # Second test (b))
 disp("Segundo Teste (b): ")
-n = int32(100);
+n = uint32(100);
 A = pentadiagMatrix(n);
 b = sum(A)';
-x = gaussSeidel(A, b, n, :, :)
+x = gaussSeidel(A, b, n, :, :, :)
 
 # Third test (c))
 disp("Terceiro Teste (c): ")
-n = int32(100);
+n = uint32(100);
 A = pentadiagMatrix(n);
 for(i=1:n)
   b(i) = 1/i;
 endfor
 b;
 ep = 1e-10;
-x = gaussSeidel(A, b, n, ep, :)
+x = gaussSeidel(A, b, n, :, ep, :)
